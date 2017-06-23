24ab2f6737b7b4042072366c5f27055d0c8c7208
beeant <312633086@qq.com>
2017-06-23 22:30:59 +0800

    客户端产品更新API站点

---
+++ Codes/Distributed/Outside/Beeant.Distributed.Outside.Version/Controllers/SharedController.cs
@@ -0,0 +1,11 @@
+﻿using Beeant.Basic.Services.Mvc.Bases;
+
+namespace Beeant.Distributed.Outside.Version.Controllers
+{
+
+    public class SharedController : SharedBaseController
+    {
+
+      
+    }
+}
---
+++ Codes/Distributed/Outside/Beeant.Distributed.Outside.Version/Models/UpdateList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Beeant.Distributed.Outside.Version.Models
+{
+    /// <summary>
+    /// 更新版本列表类
+    /// </summary>
+    [Serializable()]
+    public class UpdateList
+    {
+        /// <summary>
+        /// 可用的版本列表
+        /// </summary>
+        [XmlElement("Version")]
+        public List<ProductVersion> Version;
+    }
+    /// <summary>
+    /// 版本信息
+    /// </summary>
+    [Serializable()]
+    public class ProductVersion
+    {
+        /// <summary>
+        /// 服务器文件夹
+        /// </summary>
+        [XmlAttribute()]
+        public string Folder;
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        [XmlAttribute()]
+        public string Value;
+        /// <summary>
+        /// 忽略
+        /// </summary>
+        [XmlAttribute()]
+        public bool Ignore { get; set; }
+    }
+}

89eed7f74f30077cff30ee29ab82a0308620db78
winner <312633086@qq.com>
2017-06-26 11:27:52 +0800

    客户端更新程序

---
+++ Codes/Presentation/Client/Beeant.Presentation.Client.Launcher/FormMain.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Beeant.Presentation.Client.Launcher
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.btnLaucher = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbList = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnLaucher);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 222);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(558, 41);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Controls.Add(this.progress);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(483, 41);
+            this.panel2.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(483, 21);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // progress
+            // 
+            this.progress.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progress.Location = new System.Drawing.Point(0, 21);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(483, 20);
+            this.progress.Step = 1;
+            this.progress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progress.TabIndex = 3;
+            // 
+            // btnLaucher
+            // 
+            this.btnLaucher.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnLaucher.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnLaucher.Enabled = false;
+            this.btnLaucher.Location = new System.Drawing.Point(483, 0);
+            this.btnLaucher.Name = "btnLaucher";
+            this.btnLaucher.Size = new System.Drawing.Size(75, 41);
+            this.btnLaucher.TabIndex = 1;
+            this.btnLaucher.Text = "启动";
+            this.btnLaucher.UseVisualStyleBackColor = false;
+            this.btnLaucher.Click += new System.EventHandler(this.btnLaucher_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbList);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 222);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "文件更新列表";
+            // 
+            // rtbList
+            // 
+            this.rtbList.BackColor = System.Drawing.SystemColors.WindowText;
+            this.rtbList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbList.ForeColor = System.Drawing.Color.Lime;
+            this.rtbList.Location = new System.Drawing.Point(3, 17);
+            this.rtbList.Name = "rtbList";
+            this.rtbList.ReadOnly = true;
+            this.rtbList.Size = new System.Drawing.Size(552, 202);
+            this.rtbList.TabIndex = 1;
+            this.rtbList.Text = "";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 263);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Product AutoUpdate";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLaucher;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbList;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progress;
+    }
+}
+
---
+++ Codes/Presentation/Client/Beeant.Presentation.Client.Launcher/Utility/Extension.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Beeant.Presentation.Client.Launcher.Utility
+{
+    public static class Extension
+    {
+        #region XML
+        /// <summary>
+        /// 序列化xml
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string SerializeXml(this object obj)
+        {
+
+            try
+            {
+                if (Equals(null, obj))
+                {
+                    return null;
+                }
+                var settings = new XmlWriterSettings
+                {
+                    OmitXmlDeclaration = true,
+                    Encoding = Encoding.Default
+                };
+                //去除xml声明
+                var mem = new MemoryStream();
+                using (var writer = XmlWriter.Create(mem, settings))
+                {
+                    var ns = new XmlSerializerNamespaces();
+                    ns.Add("", "");
+                    var formatter = new XmlSerializer(obj.GetType());
+                    formatter.Serialize(writer, obj, ns);
+                }
+                return Encoding.Default.GetString(mem.ToArray());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+        /// <summary>
+        /// 反序列化xml
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static T DeserializeXml<T>(this string xml)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(xml))
+                    return default(T);
+                using (var sr = new StringReader(xml))
+                {
+                    var xmldes = new XmlSerializer(typeof(T), "");
+                    return (T)xmldes.Deserialize(sr);
+                }
+            }
+            catch //(Exception e)
+            {
+
+                return default(T);
+            }
+        }
+        #endregion
+
+        #region JSON
+        /// <summary>
+        /// 序列化json
+        /// </summary>
+        /// <param name="input"></param>
+        public static string SerializeJson(this object input)
+        {
+            try
+            {
+                return Newtonsoft.Json.JsonConvert.SerializeObject(input);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// 反序列化json
+        /// </summary>
+        /// <param name="input"></param>
+        public static T DeserializeJson<T>(this string input)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(input))
+                    return default(T);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(input);
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+        #endregion
+
+        #region 压缩与解压缩
+        /// <summary>
+        /// 压缩二进制流
+        /// </summary>
+        /// <param name="data">需要压缩的二进制流</param>
+        /// <see cref="byte"/>
+        /// <returns>压缩后二进制流</returns>
+        public static byte[] Compress(this byte[] data)
+        {
+            using (var ms = new MemoryStream())
+            {
+                try
+                {
+                    using (var zip = new DeflateStream(ms, CompressionMode.Compress, true))
+                    {
+                        zip.Write(data, 0, data.Length);
+                    }
+                    return ms.ToArray();
+                }
+                catch
+                {
+                    return null;
+                }
+
+            }
+        }
+        /// <summary>
+        /// 解压缩二进制流
+        /// </summary>
+        /// <param name="data">需要解压缩的二进制流</param>
+        /// <see cref="byte"/>
+        /// <returns>解压缩后二进制流</returns>
+        public static byte[] Decompress(this byte[] data)
+        {
+            using (var ms = new MemoryStream())
+            {
+                try
+                {
+                    ms.Write(data, 0, data.Length);
+                    ms.Flush();
+                    ms.Position = 0;
+                    using (var zip = new DeflateStream(ms, CompressionMode.Decompress, true))
+                    {
+                        using (var os = new MemoryStream())
+                        {
+                            int size = 1024;
+                            byte[] buf = new byte[size];
+                            int l = 0;
+                            do
+                            {
+                                l = zip.Read(buf, 0, size);
+                                if (l == 0) l = zip.Read(buf, 0, size);
+                                os.Write(buf, 0, l);
+                            }
+                            while (l != 0);
+                            return os.ToArray();
+                        }
+
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+
+            }
+        }
+        #endregion
+    }
+}
